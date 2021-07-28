14535c43338678a4b9c5b4691ef189ee41a6b92b
Joe Ray <jray100.jr@gmail.com>
2021-07-27 20:36:24 -0600

    Create city service spec

---
+++ spec/services/city_service_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'CityService API' do
+  it 'returns city score data correctly', :vcr do
+    data = CityService.get_city_scores('denver')
+
+    expect(data).to have_key(:categories)
+    expect(data).to have_key(:summary)
+    expect(data).to have_key(:teleport_city_score)
+
+    expect(data[:categories]).to be_an(Array)
+    expect(data[:summary]).to be_an(String)
+    expect(data[:teleport_city_score]).to be_an(Float)
+
+    expect(data[:categories].count).to eq(17)
+
+    data[:categories].each do |category|
+
+      expect(category).to have_key(:color)
+      expect(category[:color]).to be_an(String)
+
+      expect(category).to have_key(:name)
+      expect(category[:name]).to be_an(String)
+
+      expect(category).to have_key(:score_out_of_10)
+      expect(category[:score_out_of_10]).to be_an(Float)
+    end
+  end
+end
