3d6d06ff4198663527ddc8a91b519243542f8e2b
Joe Ray <jray100.jr@gmail.com>
2021-07-28 00:21:21 -0600

    Update total score test

--- spec/poros/city_spec.rb
+++ spec/poros/city_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe City do
     # city = City.new(score_data, details_data, picture_data)
 
     expect(city.summary).to eq("<p>Denver, Colorado, is among the top cities with a <b>free business environment</b>.\n\n    \n        According to our city rankings, this is a good place to live with high ratings in <b>startups</b>, <b>healthcare</b> and <b>environmental quality</b>.\n    \n\n    \n</p>\n\n\n    <p>Denver is one of the top ten city matches for 4.1% of Teleport users.</p>\n")
-    expect(city.total_score).to eq(61.413243243243265)
+    expect(city.total_score).to eq(61.41)
     expect(city.categories_array).to eq(["Housing, 3.8375",
      "Cost of Living, 5.102",
      "Startups, 7.8645",
