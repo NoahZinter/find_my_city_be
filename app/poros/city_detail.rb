a969bb952df8d07f1a77602ca4a9d84ce1e8c9bc
ahenzler <59706432+ahenzler@users.noreply.github.com>
2021-07-29 14:59:28 -0600

    add updated poros and services

--- app/poros/city_detail.rb
+++ app/poros/city_detail.rb
@@ -3,17 +3,17 @@ class CityDetail
               :summary,
               :total_score,
               :city,
-              :state
-              # :picture - won't know till we get endpoint
+              :state,
+              :picture
 
-  def initialize(data)
+  def initialize(data, photo_data)
     @categories = data[:_embedded][:"city:search-results"][0][:_embedded][:"city:item"][:_embedded][:"city:urban_area"][:_embedded][:"ua:scores"][:categories]
     @summary = data[:_embedded][:"city:search-results"][0][:_embedded][:"city:item"][:_embedded][:"city:urban_area"][:_embedded][:"ua:scores"][:summary]
     @total_score = data[:_embedded][:"city:search-results"][0][:_embedded][:"city:item"][:_embedded][:"city:urban_area"][:_embedded][:"ua:scores"][:teleport_city_score].round(2)
     @city = data[:_embedded][:"city:search-results"][0][:_embedded][:"city:item"][:_embedded][:"city:urban_area"][:name]
     @state = data[:_embedded][:"city:search-results"][0][:_embedded][:"city:item"][:_embedded][:"city:urban_area"][:_links][:"ua:admin1-divisions"][0][:name]
-    # @picture = data[:picture] - won't know till we get endpoint
-  end 
+    @picture = photo_data[:photos][0][:image][:web]
+  end
 
   def categories_hash_array
     @categories.map do |category|
