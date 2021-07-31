e17949c9980bcc44d659ac338c2808700d85a442
Regina Casias <reginaannvigil@gmail.com>
2021-07-31 17:43:02 -0600

    Took out pry

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -2,7 +2,6 @@ class WeatherFacade
 
   def self.weather(city, state_initials)
     data = WeatherService.get_weather_info(city, state_initials)
-    # binding.pry
     CityWeather.new(data)
   end
 end
