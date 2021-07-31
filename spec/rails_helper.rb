2c773d2d41a14689d0313ec588957d2a7581ad9f
Regina Casias <reginaannvigil@gmail.com>
2021-07-31 17:50:34 -0600

    Added confire for weather vcr api

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -73,7 +73,7 @@ Shoulda::Matchers.configure do |config|
   VCR.configure do |config|
     config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
     config.hook_into :webmock
-    # config.filter_sensitive_data('<moviedb_api_key>') { ENV['movie_db_api']}
+    config.filter_sensitive_data('<WEATHER_API_KEY>') { ENV['WEATHER_API_KEY']}
     config.default_cassette_options = { re_record_interval: 30.days }
     config.allow_http_connections_when_no_cassette = true
     config.configure_rspec_metadata!
@@ -211,4 +211,3 @@ def data
     "teleport_city_score": 61.413243243243265
 }
 end
-
