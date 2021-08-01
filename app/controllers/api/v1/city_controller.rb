8a0027bc9a3670bc16a9e5146d25fa9dbaafa30f
NoahZinter <noah.zinter@gmail.com>
2021-08-01 11:41:04 -0600

    Remove pry

--- app/controllers/api/v1/city_controller.rb
+++ app/controllers/api/v1/city_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::CityController < ApplicationController
   def index
     city = CityFacade.city_data(params[:city], params[:state_initials])
-    binding.pry
     render json: CitySerializer.new(city)
   end
 end
