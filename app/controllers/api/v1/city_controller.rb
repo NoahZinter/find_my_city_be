175485cd2371161963478246982b5fa13cbfa488
NoahZinter <noah.zinter@gmail.com>
2021-08-01 11:39:00 -0600

    downcase city input in service call

--- app/controllers/api/v1/city_controller.rb
+++ app/controllers/api/v1/city_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::CityController < ApplicationController
   def index
     city = CityFacade.city_data(params[:city], params[:state_initials])
+    binding.pry
     render json: CitySerializer.new(city)
   end
 end
--- app/services/city_service.rb
+++ app/services/city_service.rb
@@ -1,14 +1,12 @@
 class CityService
   class << self
     def get_city_info(city, state_initials)
-      response = conn.get("/api/cities/?search=#{city}, #{state_initials}&limit=1&embed=city:search-results/city:item/city:urban_area/ua:scores")
-
+      response = conn.get("/api/cities/?search=#{city.downcase}, #{state_initials}&limit=1&embed=city:search-results/city:item/city:urban_area/ua:scores")
       parse_json(response)
     end
 
     def get_photos_of_city(city)
-      response = conn.get("/api/urban_areas/slug:#{city}/images/")
-
+      response = conn.get("/api/urban_areas/slug:#{city.downcase}/images/")
       parse_json(response)
     end
 
