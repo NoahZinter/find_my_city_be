67fe5056ef5c141bae73a5ebfedb6888af3682ea
NoahZinter <noah.zinter@gmail.com>
2021-08-02 12:07:26 -0600

    Change controller reference to state from state intitials

--- app/controllers/api/v1/weather_contoller.rb
+++ app/controllers/api/v1/weather_contoller.rb
@@ -1,6 +1,6 @@
 class Api::V1::WeatherController < ApplicationController
   def show
-    city_weather = WeatherFacade.weather(params[:city], params[:state_initials])
+    city_weather = WeatherFacade.weather(params[:city], params[:state])
     render json: WeatherSerializer.new(city_weather)
   end
 end
