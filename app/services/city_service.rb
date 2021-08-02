729e1039116c26b34409eea0dcd03f8e10241141
NoahZinter <noah.zinter@gmail.com>
2021-08-02 11:45:46 -0600

    Remove gsub from city service

--- app/services/city_service.rb
+++ app/services/city_service.rb
@@ -1,13 +1,11 @@
 class CityService
   class << self
     def get_city_info(city, state_initials)
-      city = city.gsub(/[' ']/, '%20')
       response = conn.get("/api/cities/?search=#{city.downcase},#{state_initials}&limit=1&embed=city:search-results/city:item/city:urban_area/ua:scores")
       parse_json(response)
     end
 
     def get_photos_of_city(city)
-      city = city.gsub(/[' ']/, '%20')
       response = conn.get("/api/urban_areas/slug:#{city.downcase}/images/")
       parse_json(response)
     end
