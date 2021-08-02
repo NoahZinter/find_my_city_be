173c9accd8f4b93ed533f55f3d2572f6bd3bbb16
ignored-comment <info.konham@gmail.com>
2021-08-02 19:23:04 -0400

    Create Users controller and spec tests; assertions passing

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
 
   def create
-    @user = User.create(user_params))
+    @user = User.create(user_params)
       if @user.save
         render json: UserSerializer.new(@user), status: :created
       else
