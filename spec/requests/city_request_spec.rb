aab5dd2b2a0293f2833d3c5eb876d8d3bfe07f2d
NoahZinter <noah.zinter@gmail.com>
2021-08-03 16:43:16 -0600

    Test city search request

--- spec/requests/city_request_spec.rb
+++ spec/requests/city_request_spec.rb
@@ -4,7 +4,42 @@ RSpec.describe 'City Request' do
   describe 'index' do
     it 'searches for a city' do
       get '/api/v1/city?city=Denver&state=CO'
-      
+
+      expect(response.status).to eq 200
+
+      city = JSON.parse(response.body, symbolize_names: true)
+      city = city[:data]
+
+      expect(city[:type]).to eq 'city'
+      expect(city[:id]).is_a? String
+
+      att = city[:attributes]
+      expect(att[:id]).is_a? Integer
+      expect(att[:summary]).is_a? String
+      expect(att[:total_score]).is_a? Float
+      expect(att[:city]).to eq 'Denver'
+      expect(att[:state]).to eq 'Colorado'
+      expect(att[:categories_hash_array]).is_a? Array
+
+      cats = att[:categories_hash_array]
+
+      cats.each do |hash|
+        expect(hash[:name]).is_a? String
+        expect(hash[:score_out_of_10]).is_a? Float
+      end
+    end
+
+    it 'does not recreate created city' do
+      get '/api/v1/city?city=Denver&state=CO'
+      expect(response.status).to eq 200
+      city = JSON.parse(response.body, symbolize_names: true)
+      id = city[:data][:id]
+
+      get '/api/v1/city?city=Denver&state=CO'
+      expect(response.status).to eq 200
+      new_city = JSON.parse(response.body, symbolize_names: true)
+
+      expect(new_city[:data][:id]).to eq id
     end
   end
 end
