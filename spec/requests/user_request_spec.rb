6a9e73cd3ccf30a21746da10272cfbada5127e1f
NoahZinter <noah.zinter@gmail.com>
2021-08-03 19:25:39 -0600

    Test user creation

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -23,14 +23,25 @@ RSpec.describe 'User' do
         'password': 'food'
       }
       headers = {"CONTENT_TYPE" => "application/json"}
-
       post '/api/v1/users', headers: headers, params: JSON.generate(user: params)
-
       user = JSON.parse(response.body, symbolize_names: true)
 
       expect(user[:data][:attributes][:id]).is_a? Integer
       expect(user[:data][:attributes][:email]).is_a? String
       expect(User.find(user[:data][:attributes][:id])).is_a? User
+    end
+
+    it 'does not re-create user' do
+      orig_id = User.create!(email: 'e@mail.com', password: 'food').id
+      params = {
+        'email': 'e@mail.com',
+        'password': 'food'
+      }
+      headers = {"CONTENT_TYPE" => "application/json"}
+      post '/api/v1/users', headers: headers, params: JSON.generate(user: params)
+      user = JSON.parse(response.body, symbolize_names: true)
+
+      expect(user[:data][:attributes][:id]).to eq orig_id
     end
   end
 end
