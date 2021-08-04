f4333e579a40b34503978ea45f70b2eac7bb8af5
NoahZinter <noah.zinter@gmail.com>
2021-08-04 10:07:15 -0600

    Include new columns in favorites serializer and controller

--- app/serializers/favorite_serializer.rb
+++ app/serializers/favorite_serializer.rb
@@ -1,4 +1,4 @@
 class FavoriteSerializer
   include JSONAPI::Serializer
-  attributes :user_id, :city_id
+  attributes :user_id, :city_id, :city_name, :state, :summary, :total_score, :categories_hash_array
 end
--- spec/requests/favorites_request_spec.rb
+++ spec/requests/favorites_request_spec.rb
@@ -2,17 +2,41 @@ require 'rails_helper'
 
 RSpec.describe 'Favorites Requests' do
   describe '#create' do
+    before(:each) do
+      @categories_array = [{:name=>"Housing", :score_out_of_10=>3.8375},
+    {:name=>"Cost of Living", :score_out_of_10=>5.102},
+    {:name=>"Startups", :score_out_of_10=>7.8645},
+    {:name=>"Venture Capital", :score_out_of_10=>6.117},
+    {:name=>"Travel Connectivity", :score_out_of_10=>4.243},
+    {:name=>"Commute", :score_out_of_10=>4.530000000000001},
+    {:name=>"Business Freedom", :score_out_of_10=>8.671},
+    {:name=>"Safety", :score_out_of_10=>5.371},
+    {:name=>"Healthcare", :score_out_of_10=>8.615666666666666},
+    {:name=>"Education", :score_out_of_10=>3.6245},
+    {:name=>"Environmental Quality", :score_out_of_10=>7.11675},
+    {:name=>"Economy", :score_out_of_10=>6.5145},
+    {:name=>"Taxation", :score_out_of_10=>4.346},
+    {:name=>"Internet Access", :score_out_of_10=>5.418500000000001},
+    {:name=>"Leisure & Culture", :score_out_of_10=>6.2235},
+    {:name=>"Tolerance", :score_out_of_10=>7.860499999999999},
+    {:name=>"Outdoors", :score_out_of_10=>7.932999999999999}]
+    end
     it 'can create a favorite' do
       user_id = create(:user).id
       city_id = create(:city).id
       favorite_params = {
         user_id: user_id,
-        city_id: city_id
+        city_id: city_id,
+        city_name: 'Denver',
+        state: 'Colorado',
+        summary: 'It is a good place',
+        total_score: 55.55, 
+        categories_hash_array: @categories_array
       }
       headers = { "CONTENT_TYPE" => "application/json"}
       post '/api/v1/favorites', headers: headers, params: JSON.generate(favorite: favorite_params)
       created_favorite = Favorite.last
-
+      binding.pry
       expect(response.status).to eq 201
       expect(created_favorite.user_id).to eq user_id
       expect(created_favorite.city_id).to eq city_id
